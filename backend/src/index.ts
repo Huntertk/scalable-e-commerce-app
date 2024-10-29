86130ba30ce1b86d93fa625f78638f9e4dd7700c
Huntertk <taufik.khan65@gmail.com>
2024-10-29 15:14:20 +0530

    body parsing middleware added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,10 @@ import productRouter from './routes/products/index'
 const app = express();
 const port = 3000;
 
+//Middleware
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
